30486aa023c47f2544e086f2dc811469a3661a95
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-05-21 09:56:22 +0530

    Commit
    BasicImplementaion2 and i also downloded Brecky AssetsFile

---
+++ Assets/Script/SnakeMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject game;
+    
+    void Start()
+    {
+    
+        
+    }
+
+        void Update()
+    {
+        
+    }
+}
